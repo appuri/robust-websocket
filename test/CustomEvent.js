3a177d82c305cdf033681176aa420493a5c3452c
Nathan Black <nathan@nathanblack.org>
2016-10-10 15:28:15 -0700

    Stub out a fake CustomEvent when in a web worker

--- test/CustomEvent.js
+++ test/CustomEvent.js
@@ -8,12 +8,31 @@
         cancelable: false,
         detail: undefined
     }
-    var evt = document.createEvent('CustomEvent')
-    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail)
+
+    var evt
+    if (typeof document !== 'undefined') {
+      evt = document.createEvent('CustomEvent')
+      evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail)
+    } else {
+      // This is not a proper polyfill at all. If you need a CustomEvent in a WebWorker,
+      // please benefit the rest of the world and submit a pull request to your favorite
+      // CustomEvent polyfil like https://github.com/kaesetoast/customevent-polyfill/issues/1
+      evt = {
+        type: event,
+        detail: params.detail,
+        bubbles: false,
+        cancelable: false,
+        preventDefault: function() {},
+        stopPropagation: function() {}
+      }
+    }
+
     return evt
   }
 
-  CustomEvent.prototype = global.Event.prototype
+  if (global.Event) {
+    CustomEvent.prototype = global.Event.prototype
+  }
 
   global.CustomEvent = CustomEvent
 })(this)
